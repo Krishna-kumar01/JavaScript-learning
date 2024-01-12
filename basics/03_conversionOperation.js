cd3ee8e44affe300efdd834bbdc96bcb80820736
Krishna Kumar <kkumarmahto233@rku.ac.in>
2024-01-12 22:38:49 +0530

    change

--- basics/03_conversionOperation.js
+++ basics/03_conversionOperation.js
@@ -1,3 +1,4 @@
+/*
 let score = true
 
 console.log(typeof score);
@@ -26,3 +27,42 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+*/
+//***************************************Operations ********************
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/3);
+console.log(2%3);
+
+
+let str1 = "hello"
+let str2 = " Krishna"
+let str3 = str1 +str2
+console.log(str3);
+
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(3 + 4 * 5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+
+let num1, num2, let3
+num1 = num2 = num3 = 2 + 2
+
+
+let gameCounter = 100
+gameCouter++;
+console.log(gameCounter);
+
